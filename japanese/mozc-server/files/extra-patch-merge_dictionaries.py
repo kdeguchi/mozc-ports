8d46c9aece9a281b769679b63911fe0afacd9254
kdeguchi <kdeguchi@sz.tokoha-u.ac.jp>
2024-12-13 12:26:42 +0900

    Update fcitx-mozc and merge-ut-dictionaries

--- japanese/mozc-server/files/extra-patch-merge_dictionaries.py
+++ japanese/mozc-server/files/extra-patch-merge_dictionaries.py
@@ -1,7 +1,7 @@
---- merge_dictionaries.py.orig	2024-10-26 03:10:07.000000000 +0900
-+++ merge_dictionaries.py	2024-10-26 09:32:37.793968000 +0900
-@@ -7,18 +7,14 @@
- import gzip
+--- merge_dictionaries.py.orig	2024-12-12 16:58:48.000000000 +0900
++++ merge_dictionaries.py	2024-12-13 12:17:37.042962000 +0900
+@@ -8,18 +8,14 @@
+ import html
  import subprocess
  import sys
 -import urllib.request
@@ -14,7 +14,7 @@
 -            'google/mozc/master/src/data/dictionary_oss/id.def'
 +    id_def = open("id.def", "r")
 +    id_mozc = id_def.read()
-
+ 
 -    with urllib.request.urlopen(url) as response:
 -        id_mozc = response.read().decode()
 -
@@ -23,12 +23,12 @@
  
 @@ -62,10 +58,6 @@
  
+ 
  def count_word_hits():
-     # jawiki-latest-all-titles を取得
 -    subprocess.run(
 -        ['wget', '-N', 'https://dumps.wikimedia.org/jawiki/latest/' +
--            'jawiki-latest-all-titles-in-ns0.gz'], check=True)
--
-     with gzip.open(
-             'jawiki-latest-all-titles-in-ns0.gz', 'rt',
-             encoding='utf-8') as file:
+-            'jawiki-latest-pages-articles-multistream-index.txt.bz2'],
+-        check=True)
+ 
+     with bz2.open(
+             'jawiki-latest-pages-articles-multistream-index.txt.bz2',
