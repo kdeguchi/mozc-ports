e59b3e48f67a66363f51768c492180ccb051e8ee
kdeguchi <kdeguchi@sz.tokoha-u.ac.jp>
2025-01-29 12:45:04 +0900

    Update UT dictionaries

--- japanese/mozc-server/files/extra-patch-merge_dictionaries.py
+++ japanese/mozc-server/files/extra-patch-merge_dictionaries.py
@@ -1,27 +1,64 @@
---- merge_dictionaries.py.orig	2024-12-12 16:58:48.000000000 +0900
-+++ merge_dictionaries.py	2024-12-13 12:17:37.042962000 +0900
-@@ -8,18 +8,14 @@
- import html
+--- merge_dictionaries.py.orig	2025-01-28 05:46:28.000000000 +0900
++++ merge_dictionaries.py	2025-01-29 12:32:46.471938000 +0900
+@@ -9,9 +9,7 @@
+ import os
  import subprocess
  import sys
 -import urllib.request
  from unicodedata import normalize
+-from zipfile import ZipFile
  
  
- def get_id_mozc():
-     # Mozc の一般名詞のIDを取得
--    url = 'https://raw.githubusercontent.com/' + \
--            'google/mozc/master/src/data/dictionary_oss/id.def'
-+    id_def = open("id.def", "r")
+ def main():
+@@ -50,38 +48,18 @@
+ 
+ 
+ def get_mozc_dic():
+-    # Mozc の最終コミット日を取得
+-    url = 'https://github.com/google/mozc/commits/master/'
++    id_def  = open("id.def", "r")
 +    id_mozc = id_def.read()
++    id_mozc = id_mozc.split(' 名詞,一般,')[0].split('\n')[-1]
  
 -    with urllib.request.urlopen(url) as response:
--        id_mozc = response.read().decode()
--
-     id_mozc = id_mozc.split(' 名詞,一般,')[0].split('\n')[-1]
-     return (id_mozc)
+-        date = response.read().decode()
+-        date = date.split('"committedDate":"')[1]
+-        date = date[:10]
+-        date = date.replace('-', '')
++    # Mozc 公式辞書を取得
++    mozc_dic = []
  
-@@ -62,10 +58,6 @@
+-    # Mozc のアーカイブが古い場合は取得
+-    url = 'https://github.com/google/mozc/archive/refs/heads/master.zip'
++    for i in range(10):
++        file = open('%%WRKSRC%%/src/data/dictionary_oss/' +
++            f'dictionary0{i}.txt')
++        mozc_dic += file.read().splitlines()
+ 
+-    if os.path.exists(f'mozc-{date}.zip') is False:
+-        urllib.request.urlretrieve(
+-                url, f'mozc-{date}.zip')
+-
+-    with ZipFile(f'mozc-{date}.zip') as zip_ref:
+-        # 一般名詞のIDを取得
+-        with zip_ref.open(
+-                'mozc-master/src/data/dictionary_oss/id.def') as file:
+-            id_mozc = file.read().decode()
+-            id_mozc = id_mozc.split(' 名詞,一般,')[0].split('\n')[-1]
+-
+-        # Mozc 公式辞書を取得
+-        mozc_dic = []
+-
+-        for i in range(10):
+-            with zip_ref.open(
+-                    'mozc-master/src/data/dictionary_oss/' +
+-                    f'dictionary0{i}.txt') as file:
+-                mozc_dic += file.read().decode().splitlines()
+-
+     mozc_dic.append(id_mozc)
+     return (mozc_dic)
+ 
+@@ -121,10 +99,6 @@
  
  
  def count_word_hits():
