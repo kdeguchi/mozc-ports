e6e711718f84461402c6b9807ecfd73abb72f8a9
kdeguchi <kdeguchi@sz.tokoha-u.ac.jp>
2025-05-09 08:08:27 +0900

    Update latest

--- japanese/mozc-server/files/patch-src_ipc_unix__ipc.cc
+++ japanese/mozc-server/files/patch-src_ipc_unix__ipc.cc
@@ -4,17 +4,17 @@
  #include <sys/stat.h>
  #include <sys/time.h>
  #include <sys/un.h>
-+#if defined(__APPLE__) || defined(__FreeBSD__)
++#if defined(__FreeBSD__)
 +#include <sys/ucred.h>
 +#endif
  #include <unistd.h>
  
  #include <cerrno>
-@@ -120,7 +123,28 @@ bool IsPeerValid(int socket, pid_t *pid) {
- 
+@@ -121,6 +124,26 @@ bool IsPeerValid(int socket, pid_t *pid) {
  bool IsPeerValid(int socket, pid_t *pid) {
    *pid = 0;
-+#if defined(__APPLE__) || defined(__FreeBSD__)
+ 
++#if defined(__FreeBSD__)
 +  // If the OS is MAC, we should validate the peer by using LOCAL_PEERCRED.
 +  struct xucred peer_cred;
 +  socklen_t peer_cred_len = sizeof(struct xucred);
@@ -32,14 +32,12 @@
 +    return false;
 +  }
 +  // MacOS doesn't have cr_pid;
-+  *pid = 0;
-+#endif
- 
-+#if 0
++  *pid = peer_cred.cr_pid;
++#else
    struct ucred peer_cred;
    int peer_cred_len = sizeof(peer_cred);
    if (getsockopt(socket, SOL_SOCKET, SO_PEERCRED, &peer_cred,
-@@ -135,6 +159,7 @@ bool IsPeerValid(int socket, pid_t *pid) {
+@@ -135,6 +158,7 @@ bool IsPeerValid(int socket, pid_t *pid) {
    }
  
    *pid = peer_cred.pid;
@@ -47,11 +45,11 @@
  
    return true;
  }
-@@ -265,7 +290,12 @@ void IPCClient::Init(const absl::string_view name,
+@@ -265,7 +289,12 @@ void IPCClient::Init(const absl::string_view name,
      address.sun_family = AF_UNIX;
      absl::SNPrintF(address.sun_path, sizeof(address.sun_path), "%s",
                     server_address);
-+#if defined(__APPLE__) || defined(__FreeBSD__)
++#if defined(__FreeBSD__)
 +    address.sun_len = SUN_LEN(&address);
 +    const size_t sun_len = sizeof(address);
 +#else
@@ -60,11 +58,11 @@
      pid_t pid = 0;
      if (::connect(socket_, reinterpret_cast<const sockaddr *>(&address),
                    sun_len) != 0 ||
-@@ -378,7 +408,12 @@ IPCServer::IPCServer(const std::string &name, int32_t 
+@@ -378,7 +407,12 @@ IPCServer::IPCServer(const std::string &name, int32_t 
    int on = 1;
    ::setsockopt(socket_, SOL_SOCKET, SO_REUSEADDR, reinterpret_cast<char *>(&on),
                 sizeof(on));
-+#if defined(__APPLE__) || defined(__FreeBSD__)
++#if defined(__FreeBSD__)
 +  addr.sun_len = SUN_LEN(&addr);
 +  const size_t sun_len = sizeof(addr);
 +#else
